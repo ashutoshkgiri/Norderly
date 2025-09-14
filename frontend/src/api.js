66e7d3410b47640840e1f3472e99e1fa24fdc348
ashutoshkgiri <143159922+ashutoshkgiri@users.noreply.github.com>
2025-09-15 01:24:35 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from 'axios'
+const url=https://norderly.onrender.com
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000/api'
+  baseURL: url || 'http://localhost:4000/api'
 });
 
 API.interceptors.request.use(config => {
